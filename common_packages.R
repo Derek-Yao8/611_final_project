1ff489bfede1b476aa8358640ad893d059029c74
Derek.Yao8 <dyyao88@gmail.com>
2025-12-01 13:01:40 -0500

    Added common packages R script to load all required packages

---
+++ common_packages.R
@@ -0,0 +1,11 @@
+# R/common_packages.R
+required_pkgs <- c(
+  "tidyverse", "rvest", "janitor", "cluster",
+  "factoextra", "mclust", "aricode", "corrplot",
+  "broom", "kernlab"
+)
+
+new_pkgs <- setdiff(required_pkgs, rownames(installed.packages()))
+if (length(new_pkgs) > 0) install.packages(new_pkgs)
+
+lapply(required_pkgs, library, character.only = TRUE)
